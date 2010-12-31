5e5ee502d9a1219fefdf447a0aec2cedc1d23579
Daniel Lienert <daniel@lienert.cc>
2010-12-31 21:30:01 +0100

    ADD: Theme selector

--- ext_localconf.php
+++ ext_localconf.php
@@ -68,6 +68,6 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 	)
 );
 
-require_once t3lib_extMgm::extPath('yag').'Classes/Utility/FlexformDataProvider.php';
+require_once t3lib_extMgm::extPath('yag').'Classes/Utility/Flexform/TyposcriptDataProvider.php';
 
 ?>
