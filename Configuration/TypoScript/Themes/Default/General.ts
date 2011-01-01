f2d8f34494a14104f138543e09ed1f3e8a943444
Daniel Lienert <daniel@lienert.cc>
2011-01-01 06:43:51 +0100

    ADD: definition of resolution config in TS

---
+++ Configuration/TypoScript/Themes/Default/General.ts
@@ -0,0 +1,23 @@
+####################################################
+# Theme configuration root 
+#
+# @author Daniel Lienert <daniel@lienert.cc> Michael Knoll <knoll@punkt.de>
+# @package YAG
+# @subpackage Typoscript
+####################################################
+
+plugin.tx_yag.settings.themes.default {
+
+	resolutionConfigs {
+		thumb {
+			width = 150
+			height = 
+			quality = 
+		}
+		
+		medium {
+			width = 400
+		}
+	}
+	
+}
