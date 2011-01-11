11de52044b1f6d2f307a72bad1b1bea139bde2b1
Michael Knoll <mimi@kaktusteam.de>
2011-01-11 02:09:05 +0100

    ADD: Added some ideas to navigation controller

--- Classes/Controller/NavigationController.php
+++ Classes/Controller/NavigationController.php
@@ -31,7 +31,23 @@
  */
 class Tx_Yag_Controller_NavigationController extends Tx_Yag_Controller_AbstractController {
     
-	
+	/**
+	 * Was soll hier passieren:
+	 * 
+	 * 1. Wir benutzen ein Objekt "GalleryState" welches einen Zustand in die Session schreibt
+	 * 
+	 * 2. Wird eine Gallery ausgewählt, wir diese in den Zustand geschrieben
+	 * 2.1 Wird eine neue Gallery ausgewählt / der Gallery-Controller angezeigt, wird die Gallery im Zustand resetted
+	 * 
+	 * 3. Wird ein Album ausgewählt, wird dieses in den Zustand geschrieben
+	 * 3.1 Wird ein neues Album gewählt / der Album-List Controller angezeigt, wird das Album im Zustand resetted
+	 * 
+	 * 4. Werden weitere Filter aktiviert (Tags etc.) werden die Zustände in den Filtern festgehalten
+	 * 4.1 Der Album Zustand hat Zugriff auf die aktiven Filter
+	 * 4.2 Die Navigation erhält die Album-Filter-Breadcrumbs und kann von diesen Werte anzeigen
+	 * 
+	 * Der Zustand wird über den Lifecycle-Manager gemanaged. 
+	 */
 	
 }
  
