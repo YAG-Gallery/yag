71f368b9db16ac104a8ea338a764ddeec6de245e
Michael Knoll <mimi@kaktusteam.de>
2011-03-03 20:37:37 +0100

    FIX: #13591 for gallery index is fixed. JS messages are fading out there

--- Resources/Private/JSTemplates/GalleryBackendIndex.js
+++ Resources/Private/JSTemplates/GalleryBackendIndex.js
@@ -9,11 +9,11 @@ $(function() {
             data: "###pluginNamespace###[album]="+albumUid, 
             success: function(feedback) {
                 if(feedback=='OK') {
-                    $("#messages").html("<div class='typo3-message message-ok'>Album ist als Thumbnail festgelegt!</div>");
-                    photo.fadeOut();
+                    $("#messages").html("<div id='inner_msg' class='typo3-message message-ok'>Album ist als Thumbnail festgelegt!</div>");
                 }else{
-                    $("#messages").html("<div class='typo3-message message-error'>"+feedback+"</div>");
+                    $("#messages").html("<div id='inner_msg' class='typo3-message message-error'>"+feedback+"</div>");
                 }
+                setTimeout(function(){$('#inner_msg').fadeOut();}, 5000);
             }
         });
     });
