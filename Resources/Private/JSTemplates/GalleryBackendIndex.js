0867813014bcb658e70cd04de2205816c7caeae3
Michael Knoll <mimi@kaktusteam.de>
2011-03-04 06:47:03 +0100

    ADD: Album thumb is now half-transparent, if album is hidden. Thumb transparency does not yet change, if icon is clicked

--- Resources/Private/JSTemplates/GalleryBackendIndex.js
+++ Resources/Private/JSTemplates/GalleryBackendIndex.js
@@ -31,7 +31,8 @@ $(function() {
     function handleHideAlbumAction() {    
 	    $("a.hide-album").click(function () {
 	        var albumUid = $(this).attr("albumUid");
-	        var hideLink = $("a#hide-album-" + albumUid);
+	        var hideLink = $("a#hide-album-" + albumUid); // change icon
+	        var transparencyDiv = $("div#album-"+albumUid+"-transparency");  // make thumb half-transparent
 	        $.ajax({
 	            url: hidealbum_url,
 	            data: "###pluginNamespace###[album]="+albumUid, 
@@ -39,6 +40,7 @@ $(function() {
 	                if(feedback=='OK') {
 	                    $("#messages").html("<div id='inner_msg' class='typo3-message message-ok'>Ihr Album wurde auf 'unsichtbar' gesetzt!</div>");
 	                    hideLink.replaceWith('<a id="unhide-album-'+albumUid+'" class="unhide-album" albumUid="'+albumUid+'"><span class="t3-icon t3-icon-actions t3-icon-actions-edit t3-icon-edit-unhide">&nbsp;</span></a>');
+	                    transparencyDiv.css('filter:alpha(opacity=50);-moz-opacity:0.5;-khtml-opacity: 0.5;opacity: 0.5;');
 	                    handleUnhideAlbumAction(); // call handler for element just created
 	                }else{
 	                    $("#messages").html("<div id='inner_msg' class='typo3-message message-error'>"+feedback+"</div>");
@@ -53,7 +55,8 @@ $(function() {
     function handleUnhideAlbumAction() {    
 	    $("a.unhide-album").click(function () {
 	        var albumUid = $(this).attr("albumUid");
-	        var unhideLink = $("a#unhide-album-" + albumUid);
+	        var unhideLink = $("a#unhide-album-" + albumUid); // change icon
+	        var transparencyDiv = $("div#album-"+albumUid+"-transparency"); // make thumb half-transparent
 	        $.ajax({
 	            url: unhidealbum_url,
 	            data: "###pluginNamespace###[album]="+albumUid, 
@@ -61,6 +64,7 @@ $(function() {
 	                if(feedback=='OK') {
 	                    $("#messages").html("<div id='inner_msg' class='typo3-message message-ok'>Ihr Album wurde auf 'sichtbar' gesetzt!</div>");
 	                    unhideLink.replaceWith('<a id="hide-album-'+albumUid+'" class="hide-album" albumUid="'+albumUid+'"><span class="t3-icon t3-icon-actions t3-icon-actions-edit t3-icon-edit-hide">&nbsp;</span></a>');
+	                    transparencyDiv.css('');
 	                    handleHideAlbumAction(); // call handler for element just created
 	                }else{
 	                    $("#messages").html("<div id='inner_msg' class='typo3-message message-error'>"+feedback+"</div>");
