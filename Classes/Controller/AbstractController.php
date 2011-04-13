88b7a74d9d1ee914a2a613e93782a1bd84c0741a
Daniel Lienert <daniel@lienert.cc>
2011-04-13 23:10:24 +0200

    CHG: Changed AbstractController to made YAG compatible to next version of pt_extlist

--- Classes/Controller/AbstractController.php
+++ Classes/Controller/AbstractController.php
@@ -99,15 +99,7 @@ abstract class Tx_Yag_Controller_AbstractController extends Tx_Extbase_MVC_Contr
     
 
     public function __construct() {
-    	if(TYPO3_MODE === 'FE') { 
-    		t3lib_div::makeInstance('Tx_Extbase_Object_ObjectManager')->get('Tx_PtExtlist_Extbase_ExtbaseContext')->setInCachedMode(true);
-    		$sessionStorageMode = Tx_PtExtlist_Domain_StateAdapter_SessionPersistenceManager::STORAGE_ADAPTER_NULL;
-    		t3lib_div::makeInstance('Tx_Extbase_Object_ObjectManager')->get('Tx_PtExtlist_Extbase_ExtbaseContext')->setSessionStorageMode(Tx_PtExtlist_Domain_StateAdapter_SessionPersistenceManager::STORAGE_ADAPTER_NULL);
-    	}
-    	
     	$this->lifecycleManager = Tx_PtExtlist_Domain_Lifecycle_LifecycleManagerFactory::getInstance();
-		$this->lifecycleManager->registerAndUpdateStateOnRegisteredObject(Tx_PtExtlist_Domain_StateAdapter_SessionPersistenceManagerFactory::getInstance($sessionStorageMode));
-		
 		parent::__construct();
     }
     
@@ -230,24 +222,30 @@ abstract class Tx_Yag_Controller_AbstractController extends Tx_Extbase_MVC_Contr
 		
     	$contextIdentifier = $this->getContextIdentifier();
     	
-        if($this->settings != NULL) {
-	        $this->emSettings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['yag']);
-	        
-	        Tx_Yag_Domain_Configuration_ConfigurationBuilderFactory::injectSettings($this->settings);
-	        $this->configurationBuilder = Tx_Yag_Domain_Configuration_ConfigurationBuilderFactory::getInstance($contextIdentifier, $this->settings['theme']);
+    	if($this->settings != NULL) {
+    		$this->emSettings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['yag']);
+    		 
+    		Tx_Yag_Domain_Configuration_ConfigurationBuilderFactory::injectSettings($this->settings);
+    		$this->configurationBuilder = Tx_Yag_Domain_Configuration_ConfigurationBuilderFactory::getInstance($contextIdentifier, $this->settings['theme']);
 
-          /**
-           * As we have configuration builder as a singleton, we cannot determine flexform settings
-           * if there are multiple instances of a plugin on the same page.
-           * 
-           * So we have to pass plugin-instance specific settings via direct access to settings
-           * here
-           * 
-           * YOU SHOULD TAKE THIS SERIOUSLY!
-           */
- 
-	       $this->yagContext = Tx_Yag_Domain_Context_YagContextFactory::createInstance($contextIdentifier);
-        }
+    		if(TYPO3_MODE === 'FE') { 
+    			t3lib_div::makeInstance('Tx_Extbase_Object_ObjectManager')->get('Tx_PtExtlist_Extbase_ExtbaseContext')->setInCachedMode(true);
+				$sessionStorageClass = Tx_PtExtlist_Domain_StateAdapter_SessionPersistenceManager::STORAGE_ADAPTER_NULL;
+				
+				// support old pt_extlist - remove, if this version requires the newer pt_extlist version 
+				if(method_exists(t3lib_div::makeInstance('Tx_Extbase_Object_ObjectManager')->get('Tx_PtExtlist_Extbase_ExtbaseContext'), 'setSessionStorageMode')) {
+					t3lib_div::makeInstance('Tx_Extbase_Object_ObjectManager')->get('Tx_PtExtlist_Extbase_ExtbaseContext')->setSessionStorageMode(Tx_PtExtlist_Domain_StateAdapter_SessionPersistenceManager::STORAGE_ADAPTER_NULL);
+				}
+			} else {
+				$sessionStorageClass = Tx_PtExtlist_Domain_StateAdapter_SessionPersistenceManager::STORAGE_ADAPTER_BROWSER_SESSION;
+			}		
+			
+			
+			$this->lifecycleManager->registerAndUpdateStateOnRegisteredObject(Tx_PtExtlist_Domain_StateAdapter_SessionPersistenceManagerFactory::getInstance($sessionStorageClass));
+			
+    		
+    		$this->yagContext = Tx_Yag_Domain_Context_YagContextFactory::createInstance($contextIdentifier);
+    	}
     }
     
     
