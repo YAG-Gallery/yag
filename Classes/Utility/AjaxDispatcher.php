d969f902c57a9578d50697eef232543812e151e8
Daniel Lienert <daniel@lienert.cc>
2011-04-19 12:42:35 +0200

    Dispatcher now gets its config from ajax call

--- Classes/Utility/AjaxDispatcher.php
+++ Classes/Utility/AjaxDispatcher.php
@@ -41,17 +41,41 @@ class Tx_Yag_Utility_AjaxDispatcher {
 	protected $objectManager;
 	
 	
+	/**
+	 * @var string
+	 */
 	protected $extensionName;
 	
+	
+	/**
+	 * @var string
+	 */
 	protected $pluginName;
 	
+	
+	/**
+	 * @var string
+	 */
 	protected $controllerName;
 	
+	
+	/**
+	 * @var string
+	 */
 	protected $actionName;
 	
+	
+	/**
+	 * @var array
+	 */
 	protected $arguments;
 	
 	
+	
+	/**
+	 * Called by ajax.php / eID.php
+	 * Builds an extbase context and returns the response
+	 */
 	public function dispatch() {
 		$this->prepareCallArguments();
 		
@@ -63,49 +87,51 @@ class Tx_Yag_Utility_AjaxDispatcher {
 		$bootstrap->initialize($configuration);
 		
 		$this->objectManager = t3lib_div::makeInstance('Tx_Extbase_Object_ObjectManager');
-		
-		$request = $this->objectManager->get('Tx_Extbase_MVC_Web_Request'); /* @var $request Tx_Extbase_MVC_Request */
-		$request->setControllerExtensionName($this->extensionName);
-		$request->setPluginName($this->actionName);
-		$request->setControllerName($this->controllerName);
-		$request->setControllerActionName($this->actionName);
-		
+
+		$request = $this->buildRequest();
 		$response = $this->objectManager->create('Tx_Extbase_MVC_Web_Response');
 		
 		$dispatcher =  $this->objectManager->get('Tx_Extbase_MVC_Dispatcher');
 		$dispatcher->dispatch($request, $response);
 
 		echo $response->getContent();
-	
-		die('DONE');
 	}
 
 	
 	
-	protected function prepareCallArguments() {
-		$this->extensionName = 'Yag';
-		$this->pluginName = 'pi1';
-		$this->controllerName = 'Item';
-		$this->actionName = 'showSingle';
-		$this->arguments = array('item' => 1);
-	}
-	
-	
-	
-	protected function getTyposcriptSettings($pid) {
-		$typoScript = tx_pttools_div::returnTyposcriptSetup($pid, 'plugin.tx_yag.settings.');
-		return $typoScript;
-	}
-	
-	
-	protected function determineCurrentPID() {
-		$pid = (int) t3lib_div::_GP('PID');
+	/**
+	 * Build a request object
+	 * 
+	 * @return Tx_Extbase_MVC_Web_Request $request
+	 */
+	protected function buildRequest() {
+		$request = $this->objectManager->get('Tx_Extbase_MVC_Web_Request'); /* @var $request Tx_Extbase_MVC_Request */
+		$request->setControllerExtensionName($this->extensionName);
+		$request->setPluginName($this->actionName);
+		$request->setControllerName($this->controllerName);
+		$request->setControllerActionName($this->actionName);
+		$request->setArguments($this->arguments);
 		
-		return $pid;
+		return $request;
 	}
 	
 	
-
 	
+	/**
+	 * Prepate the call arguments
+	 * @TODO escape / unescape values ?
+	 */
+	protected function prepareCallArguments() {
+		$callJSON = t3lib_div::_GP('call');
+		
+		//http://t3develop.harper/typo3/ajax.php?ajaxID=yagAjaxDispatcher&id=22&call={%22extensionName%22:%22Yag%22,%22pluginName%22:%22pi1%22,%22controllerName%22:%22Item%22,%22actionName%22:%22showSingle%22,%22arguments%22:{%22item%22:1}}
+		
+		$call = json_decode($callJSON);
+		$this->extensionName 	= $call['extensionName'];
+		$this->pluginName 		= $call['pluginName'];
+		$this->controllerName 	= $call['controllerName'];
+		$this->actionName 		= $call['showSingle'];
+		$this->arguments 		= $call['arguments'];	
+	}
 }
 ?>
