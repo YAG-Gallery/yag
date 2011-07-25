22109a1a4fb16726aff092b169fd33258a56fd50
Michael Knoll <mimi@kaktusteam.de>
2011-07-25 06:44:13 +0200

    ADD: Added some documentation and a readme for updating

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'cms' => '',
 			'extbase' => '1.3.0',
 			'fluid' => '1.3.0',
-			'pt_extlist' => '0.2.6-pt_extbase_rft',
+			'pt_extlist' => '0.3.0',
 			'typo3' => '4.5.0-0.0.0',
 		),
 		'conflicts' => array(
