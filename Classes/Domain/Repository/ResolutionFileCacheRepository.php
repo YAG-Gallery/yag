d253d156f0a6c7e45dc675591ac3d48cbce6c7e6
Michael Knoll <mimi@kaktusteam.de>
2011-10-01 02:33:19 +0200

    FIX: Fixed bug 30489, setting raw query results to true will never be reset. Removed resolution file cache quality from TCA.

--- Classes/Domain/Repository/ResolutionFileCacheRepository.php
+++ Classes/Domain/Repository/ResolutionFileCacheRepository.php
@@ -33,7 +33,6 @@
  */
 class Tx_Yag_Domain_Repository_ResolutionFileCacheRepository extends Tx_Extbase_Persistence_Repository {
 
-
 	/**
 	 * Constructor of the repository.
 	 * Sets the respect storage page to false.
@@ -43,8 +42,8 @@ class Tx_Yag_Domain_Repository_ResolutionFileCacheRepository extends Tx_Extbase_
 		 parent::__construct($objectManager);
 		 $this->defaultQuerySettings = new Tx_Extbase_Persistence_Typo3QuerySettings();
 		 $this->defaultQuerySettings->setRespectStoragePage(FALSE);
-		 $this->defaultQuerySettings->setRespectStoragePage(FALSE);
 	}
+
 
 		
 	/**
@@ -65,12 +64,13 @@ class Tx_Yag_Domain_Repository_ResolutionFileCacheRepository extends Tx_Extbase_
 		$result = $query->matching($query->logicalAnd($constraints))->execute();
 
 		$object = NULL;
-		if ($result !== NULL && $result->current() !== FALSE) {
+		if ($result !== NULL && !is_array($result) && $result->current() !== FALSE) {
 			$object = $result->current();
 			$this->identityMap->registerObject($object, $object->getUid());
 		}
 		return $object;
 	}
+
 
 
 	/**
@@ -105,8 +105,11 @@ class Tx_Yag_Domain_Repository_ResolutionFileCacheRepository extends Tx_Extbase_
 			}
 		}
 
+        $this->defaultQuerySettings->setReturnRawQueryResult(FALSE);
+
 		return $fileCacheArray;
 	}
+
 	
 	
 	/**
@@ -150,5 +153,6 @@ class Tx_Yag_Domain_Repository_ResolutionFileCacheRepository extends Tx_Extbase_
 		$itemsInRepository = $this->addedObjects->count();
 		return $itemsInDatabase + $itemsInRepository;
 	}
+
 }
 ?>
