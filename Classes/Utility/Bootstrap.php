d48f2f3f64f9456d6354f34f867e2bfc12f64998
Daniel Lienert <daniel@lienert.cc>
2011-11-15 22:53:12 +0100

    ADD: Bootstrap class. The method boot() loads the configuration and makes the extension usable in third party extensions

---
+++ Classes/Utility/Bootstrap.php
@@ -0,0 +1,83 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Daniel Lienert <lienert@punkt.de>
+*  All rights reserved
+*
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+class Tx_Yag_Utility_Bootstrap implements t3lib_Singleton {
+
+	/**
+	 * @var string
+	 */
+	protected $theme = 'default';
+
+
+	/**
+	 * @var string
+	 */
+	protected $contextIdentifier = 'extUsage';
+
+
+	/**
+	 * @return void
+	 */
+	public function boot() {
+
+		$this->initConfigurationBuilder();
+
+	}
+
+
+
+	/**
+	 * @return void
+	 */
+	protected function initConfigurationBuilder() {
+
+		$yagSettings = Tx_PtExtbase_Div::typoscriptRegistry('plugin.tx_yag.settings.');
+		$yagEBSettings = Tx_Extbase_Utility_TypoScript::convertTypoScriptArrayToPlainArray($yagSettings);
+
+		Tx_Yag_Domain_Configuration_ConfigurationBuilderFactory::injectSettings($yagEBSettings);
+		Tx_Yag_Domain_Configuration_ConfigurationBuilderFactory::getInstance($this->contextIdentifier, $this->theme);
+
+	}
+
+
+
+	/**
+	 * @param  $theme
+	 */
+	public function setTheme($theme) {
+		$this->theme = $theme;
+	}
+
+
+	
+	/**
+	 * @param string $contextIdentifier
+	 */
+	public function setContextIdentifier($contextIdentifier) {
+		$this->contextIdentifier = $contextIdentifier;
+	}
+}
+
+?>
