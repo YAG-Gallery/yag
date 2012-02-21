d9f897cca3cce48e97bb534593294eb60fedc200
Daniel Lienert <daniel@lienert.cc>
2012-02-21 22:40:49 +0100

    CHG: Title now fades in when you hover over the image

--- Resources/Private/JSTemplates/FancyBox.js
+++ Resources/Private/JSTemplates/FancyBox.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-	/* Apply fancybox to multiple items */
-
 	$("a.fancybox").fancybox({
         'transitionIn': 'elastic',
         'transitionOut': 'elastic',
@@ -10,8 +8,18 @@ $(document).ready(function() {
         'padding': 0,
         'overlayColor':'#000',
         'overlayOpacity':0.8,
+
         'titleFormat': function(title, currentArray, currentIndex, currentOpts) {
-            return '<span id="fancybox-title-over">'+ (title.length ? '' + title : '') + '</span>';
+            return '<span id="fancybox-title-over">'+ (title.length ? '' + title : '') +  'Image ' + (currentIndex + 1) + ' of ' + currentArray.length + '</span>';
+        },
+
+        'onComplete': function() {
+            $("#fancybox-title").hide();
+            $("#fancybox-wrap").hover(function() {
+                $("#fancybox-title").fadeIn("fast");
+            }, function() {
+                $("#fancybox-title").fadeOut("fast");
+            });
         }
 	});
 
