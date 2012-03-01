6ea66a9c816e4e25bda36e483437d35f3802bae5
Michael Knoll <mimi@kaktusteam.de>
2012-03-01 04:34:22 +0100

    FIX: Icon for yag folder should now be rendered

--- ext_tables.php
+++ ext_tables.php
@@ -239,7 +239,7 @@ $TCA['tx_yag_domain_model_tag'] = array(
 );
 
 // Register yag for 'contains plugin' in sysfolders
-$TCA['pages']['columns']['module']['config']['items'][] = array('yag', 'yag', 'EXT:yag/ext_icon.gif');
+$TCA['pages']['columns']['module']['config']['items'][] = array('LLL:EXT:yag/Resources/Private/Language/locallang.xml:general.yag', 'yag', 'i/ext_icon.gif');
 t3lib_SpriteManager::addTcaTypeIcon('pages', 'contains-yag', '../typo3conf/ext/yag/ext_icon.gif');
 
 ?>
