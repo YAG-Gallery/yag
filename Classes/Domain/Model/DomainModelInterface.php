c0430da9b8794cc9e5185ef67b51850e7dd7933e
Michael Knoll <mimi@kaktusteam.de>
2012-10-03 22:52:41 +0200

    FIX: Fixed issue 34669. Some bigger changes happened here. All domain objects get new interface for being able to determine the right pid

---
+++ Classes/Domain/Model/DomainModelInterface.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mimi
+ * Date: 04.10.12
+ * Time: 16:20
+ * To change this template use File | Settings | File Templates.
+ */
+interface Tx_Yag_Domain_Model_DomainModelInterface {
+
+}
