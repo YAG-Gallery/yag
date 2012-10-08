edb2d60bea0ff461eeaf7d9e25ab7ce3ba14fdbb
Daniel Lienert <daniel@lienert.cc>
2012-10-08 12:54:19 +0200

    ADD: option to add a perma link to images

--- Configuration/TypoScript/Themes/Lightbox/ItemList.ts
+++ Configuration/TypoScript/Themes/Lightbox/ItemList.ts
@@ -11,7 +11,8 @@ plugin.tx_yag.settings.themes.lightbox.itemList {
 	itemsPerPage = 12
 	columnCount = 4
 	showTitle = 0
-	
+
+
     ## Set partial used for rendering an image thumbnail
     imageThumbPartial = Image/LightboxImageThumb
 }
