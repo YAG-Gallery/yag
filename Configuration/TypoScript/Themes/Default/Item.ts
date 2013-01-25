2c1f9cb8721ebda5136695e2b47cdda7f43c5b7a
Daniel Lienert <daniel@lienert.cc>
2013-01-25 13:25:00 +0100

    ADD: additionalPartials as "partialHook" for the itemTemplate

--- Configuration/TypoScript/Themes/Default/Item.ts
+++ Configuration/TypoScript/Themes/Default/Item.ts
@@ -13,14 +13,27 @@ plugin.tx_yag.settings.themes.default.item {
 	
 	# Show the item description beneath the image
 	showDescription = 0
-	
+
+	# Show the back / forward pager
+	showPager = 1
+
 	# Show Meta information for an item (including title and description)
 	showItemMeta = 1
 	
 	# Show downloadlink to original item
 	showOriginalDownloadLink = 1
-	
+
+	# Path to pager partial
+	pagerPartial = Pager/SingleItem
+
 	# Path to item meta partial
 	itemMetaPartial = Image/ImageMeta
 
+
+	additionalPartials {
+		10  {
+			path = ExternalPlugins/ThRating
+			show = 1
+		}
+	}
 }
