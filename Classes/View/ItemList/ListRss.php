ebd29f20e6585df8956724178229acaeaed760f5
Daniel Lienert <daniel@lienert.cc>
2013-02-05 06:00:44 +0100

    TASK: Refactor Rss

---
+++ Classes/View/ItemList/ListRss.php
@@ -0,0 +1,15 @@
+<?php
+
+class Tx_PtExtlist_View_BaseView extends Tx_PtExtbase_View_BaseView {
+
+
+	/**
+	 * (non-PHPdoc)
+	 * @see Classes/View/Tx_Fluid_View_TemplateView::initializeView()
+	 */
+	public function initializeView() {
+		die('BAM');
+	}
+
+
+}
