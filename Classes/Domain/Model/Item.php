b25af8b7ea6d8ee106498e980e640f9eec0a819c
Daniel Lienert <lienert@punkt.de>
2013-10-24 15:44:42 +0200

    FIX: Injects seem to not work on models created from a repository query - manually create objects on __wakeup()

--- Classes/Domain/Model/Item.php
+++ Classes/Domain/Model/Item.php
@@ -223,7 +223,7 @@ class Tx_Yag_Domain_Model_Item
 	}
 
 
-	public function __wakeup() {
+	public function __wakeUp() {
 		if(!$this->objectManager instanceof Tx_Extbase_Object_ObjectManager) $this->objectManager = t3lib_div::makeInstance('Tx_Extbase_Object_ObjectManager'); // TYPO3 4.5 Fix
 	}
 
