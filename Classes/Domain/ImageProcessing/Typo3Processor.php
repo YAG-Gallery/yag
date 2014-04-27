704a529b2dcd92c0cf80e38a468e4d2177685cda
Daniel Lienert <daniel@lienert.cc>
2014-04-27 15:51:33 +0200

    [BUGFIX] Bug #57955 Call to undefined method stdClass::set_no_cache()

--- Classes/Domain/ImageProcessing/Typo3Processor.php
+++ Classes/Domain/ImageProcessing/Typo3Processor.php
@@ -151,27 +151,35 @@ class Tx_Yag_Domain_ImageProcessing_Typo3Processor extends Tx_Yag_Domain_ImagePr
 			$imageResource = $contentObject->getImgResource($sourcePathAndFileName, $typoScriptSettings);
 		}
 
+		$this->typo3CleanUp($imageResource, $sourcePathAndFileName);
+
 		return $imageResource;
 	}
 
 
-
 	/**
-     * As we have our own resolution file cache system
-     * we dont want to polute the TYPO3 cache_imagesizes table.
-     * So we remove the generated image (messy, but the only way ...)
-     * 
-     * @param $imageResource filename to remove from table
-     */
-    protected function typo3CleanUp($imageResource) {
-    	$GLOBALS['TYPO3_DB']->exec_DELETEquery(
+	 * As we have our own resolution file cache system
+	 * we dont want to polute the TYPO3 cache_imagesizes table.
+	 * So we remove the generated image (messy, but the only way ...)
+	 *
+	 * @param $imageResource filename to remove from table
+	 * @param $originalFileName
+	 */
+	protected function typo3CleanUp($imageResource, $originalFileName) {
+
+		$GLOBALS['TYPO3_DB']->exec_DELETEquery(
 			'cache_imagesizes',
-			'filename=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($imageResource[3], 'cache_imagesizes')
-    	);
-    	
-    	unset($GLOBALS['TSFE']->tmpl->fileCache[$imageResource['fileCacheHash']]);
-    }
-    
+			'filename = ' . $GLOBALS['TYPO3_DB']->fullQuoteStr($imageResource[3], 'cache_imagesizes')
+		);
+
+		$GLOBALS['TYPO3_DB']->exec_DELETEquery(
+			'cache_typo3temp_log',
+			'orig_filename = ' . $GLOBALS['TYPO3_DB']->fullQuoteStr($this->fileSystemDiv->makePathAbsolute($originalFileName), 'cache_typo3temp_log')
+		);
+
+		unset($GLOBALS['TSFE']->tmpl->fileCache[$imageResource['fileCacheHash']]);
+	}
+
     
     
 	/**
