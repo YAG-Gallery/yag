1e34a3133d072a4d1c18200ac866a8afa4196d8c
Daniel Lienert <daniel@lienert.cc>
2014-06-21 05:07:51 +0200

    RFT: ext_autoload to namespaces

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,19 +1,19 @@
 <?php
 // This class contains information about classes to be registered in autoload for testing
 
-$extensionTestsPath = t3lib_extMgm::extPath('yag') . 'Tests/';
-$extbasePath = t3lib_extMgm::extPath('extbase');
+$extensionTestsPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('yag') . 'Tests/';
+$extbasePath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('extbase');
 return array(
     'tx_extbase_basetestcase' => $extbasePath . 'Tests/Unit/BaseTestCase.php',
     'tx_yag_tests_basetestcase' => $extensionTestsPath . 'BaseTestCase.php',
     'tx_yag_tests_defaulttsconfig' => $extensionTestsPath . 'DefaultTsConfig.php',
     
-    'user_tx_yag_utility_flexformdataprovider' => t3lib_extMgm::extPath('yag').'Classes/Utility/FlexformDataProvider.php',
-	'user_tx_yag_utility_flexform_typoscriptdataprovider' => t3lib_extMgm::extPath('yag').'Classes/Utility/TyposcriptDataProvider.php',
-    'user_tx_yag_hooks_realurl' => t3lib_extMgm::extPath('yag').'Classes/Hooks/RealUrlHook.php',
+    'user_tx_yag_utility_flexformdataprovider' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('yag').'Classes/Utility/FlexformDataProvider.php',
+	'user_tx_yag_utility_flexform_typoscriptdataprovider' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('yag').'Classes/Utility/TyposcriptDataProvider.php',
+    'user_tx_yag_hooks_realurl' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('yag').'Classes/Hooks/RealUrlHook.php',
 	
-    'tx_yag_report_externallibraries' => t3lib_extMgm::extPath('yag').'Classes/Report/ExternalLibraries.php',
-    'tx_yag_report_environmentvariables' => t3lib_extMgm::extPath('yag').'Classes/Report/EnvironmentVariables.php',
-    'tx_yag_report_filesystem' => t3lib_extMgm::extPath('yag').'Classes/Report/Filesystem.php',
+    'tx_yag_report_externallibraries' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('yag').'Classes/Report/ExternalLibraries.php',
+    'tx_yag_report_environmentvariables' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('yag').'Classes/Report/EnvironmentVariables.php',
+    'tx_yag_report_filesystem' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('yag').'Classes/Report/Filesystem.php',
 );
 ?>
