6e657350e95b1984d57ddc12935ea6cdf6a00668
Daniel Lienert <daniel@lienert.cc>
2014-07-10 23:10:14 +0200

    [BUGFIX] Fix Bug #57961: Gallery edit link now correctly sets the gallery in the session state, so that the correct albums are shown in the album list

--- Classes/ViewHelpers/Link/GalleryViewHelper.php
+++ Classes/ViewHelpers/Link/GalleryViewHelper.php
@@ -2,7 +2,7 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2010-2013 Daniel Lienert <daniel@lienert.cc>, Michael Knoll <mimi@kaktusteam.de>
+*  (c) 2010-2014 Daniel Lienert <daniel@lienert.cc>
 *  All rights reserved
 *
 *
@@ -24,40 +24,41 @@
 ***************************************************************/
 
 /**
- * Class implements a viewhelper for rendering a link for an gallery
+ * Class implements a viewhelper for rendering a link to a gallery
  *
  * @package ViewHelpers
  * @author Daniel Lienert <daniel@lienert.cc>
- * @author Michael Knoll <mimi@kaktusteam.de>
  */
 class Tx_Yag_ViewHelpers_Link_GalleryViewHelper extends Tx_PtExtlist_ViewHelpers_Link_ActionViewHelper {
 
-    /**
-     * Renders link for an album
-     *
-     * @param int $galleryUid UID of album to render link for
-     * @param Tx_Yag_Domain_Model_Gallery $gallery Album object to render link for
-     * @param int $pageUid (Optional) ID of page to render link for. If null, current page is used
-     * @return string Rendered link for gallery
+	/**
+	 * Renders link for an album
 	 *
+	 * @param int $galleryUid UID of album to render link for
+	 * @param Tx_Yag_Domain_Model_Gallery $gallery Album object to render link for
+	 * @param int $pageUid (Optional) ID of page to render link for. If null, current page is used
+	 * @param int $pageType
+	 * @param bool $noCache
+	 * @param bool $noCacheHash
+	 * @param string $section
+	 * @param string $format
 	 * @throws Exception
-     */
-	public function render($galleryUid = NULL, Tx_Yag_Domain_Model_Gallery $gallery = NULL, $pageUid = NULL) {
-        if ($galleryUid === NULL && $gallery === NULL) {
-            throw new Exception('You have to set "galleryUid" or "gallery" as parameter. Both parameters can not be empty when using galleryLinkViewHelper 1295575455');
-        }
-        if ($galleryUid === NULL) {
-            $galleryUid = $gallery->getUid();
-        }
-       
-        $namespace = Tx_Yag_Domain_Context_YagContextFactory::getInstance()->getObjectNamespace().'.galleryUid';
+	 * @return string Rendered link for gallery
+	 *
+	 */
+	public function render($galleryUid = NULL, Tx_Yag_Domain_Model_Gallery $gallery = NULL, $pageUid = NULL, $pageType = 0, $noCache = FALSE, $noCacheHash = FALSE, $section = '', $format = '') {
+		if ($galleryUid === NULL && $gallery === NULL) {
+			throw new Exception('You have to set "galleryUid" or "gallery" as parameter. Both parameters can not be empty when using galleryLinkViewHelper', 1295575455);
+		}
+		if ($galleryUid === NULL) {
+			$galleryUid = $gallery->getUid();
+		}
+
+		$namespace = Tx_Yag_Domain_Context_YagContextFactory::getInstance()->getObjectNamespace() . '.galleryUid';
 		$arguments = Tx_PtExtbase_Utility_NameSpace::saveDataInNamespaceTree($namespace, array(), $galleryUid);
 
-        Tx_PtExtbase_State_Session_SessionPersistenceManagerFactory::getInstance()->addSessionRelatedArguments($arguments);
-		
-        return parent::render('index', $arguments, 'Gallery', NULL, NULL, $pageUid);
-    }
-    
+		Tx_PtExtbase_State_Session_SessionPersistenceManagerFactory::getInstance()->addSessionRelatedArguments($arguments);
+
+		return parent::render('index', $arguments, 'Gallery', NULL, NULL, $pageUid, $pageType, $noCache, $noCacheHash, $section, $format);
+	}
 }
- 
-?>
