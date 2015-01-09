5e93694ca72bac8e7611b301a68851ddf14360d5
Daniel Lienert <lienert@punkt.de>
2015-01-09 13:29:51 +0100

    [BUGFIX] Fix path to imageNotFound sysImage

--- Configuration/TypoScript/BaseConfig/SysImages.ts
+++ Configuration/TypoScript/BaseConfig/SysImages.ts
@@ -10,7 +10,7 @@
 config.tx_yag.settings {
 	sysImages {
 		imageNotFound {
-			sourceUri = typo3conf/yag/Resources/Public/Icons/imageNotFound.jpg
+			sourceUri = typo3conf/ext/yag/Resources/Public/Icons/imageNotFound.jpg
 			title = No image found.
 			description = No image found.
 		}
