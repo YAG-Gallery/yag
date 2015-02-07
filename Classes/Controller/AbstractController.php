06f54268db63fcb5e258b92ceaa860366aea87a6
Markus Klein <markus.klein@reelworx.at>
2015-02-07 11:35:58 +0100

    [BUGFIX] Add missing backslash escapes in strings

--- Classes/Controller/AbstractController.php
+++ Classes/Controller/AbstractController.php
@@ -396,7 +396,7 @@ abstract class Tx_Yag_Controller_AbstractController extends Tx_PtExtbase_Control
 	protected function getContextIdentifier() {
 
 		// Stage 1: get the identifier from GET / POST
-		$identifier = GeneralUtility::makeInstance('TYPO3\\CMS\Extbase\\Object\\ObjectManager')
+		$identifier = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager')
 			->get('Tx_PtExtlist_Domain_StateAdapter_GetPostVarAdapterFactory')->getInstance()->extractGpVarsByNamespace('contextIdentifier');
 
 		// Stage 2: get a defined identifier
--- Classes/Report/Filesystem.php
+++ Classes/Report/Filesystem.php
@@ -87,7 +87,7 @@ class Tx_Yag_Report_Filesystem implements \TYPO3\CMS\Reports\StatusProviderInter
 					);
 				}
 			} else {
-				$this->reports[] = GeneralUtility::makeInstance('TYPO3\CMS\Reports\Status',
+				$this->reports[] = GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status',
 					'Filesystem',
 					'Original files root',
 					'The directory to store original images (' . $extConfSettings['origFilesRoot'] . ') does not exist!',
@@ -95,7 +95,7 @@ class Tx_Yag_Report_Filesystem implements \TYPO3\CMS\Reports\StatusProviderInter
 				);
 			}
 		} else {
-			$this->reports[] = GeneralUtility::makeInstance('TYPO3\CMS\Reports\Status',
+			$this->reports[] = GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status',
 				'Filesystem',
 				'Original files root',
 				'The directory to store original images has not been set in Extension Manager!',
@@ -112,28 +112,28 @@ class Tx_Yag_Report_Filesystem implements \TYPO3\CMS\Reports\StatusProviderInter
 	protected function checkYagTmpDirectory() {
 		$extConfSettings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['yag']);
 		if (array_key_exists('hashFilesystemRoot', $extConfSettings)) {
-			$this->reports[] = GeneralUtility::makeInstance('TYPO3\CMS\Reports\Status',
+			$this->reports[] = GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status',
 				'Filesystem',
 				'Hash filesystem root',
 				'Setting for hash filesystem root is available.',
 				Status::OK
 			);
 			if (is_dir(PATH_site . '/' . $extConfSettings['hashFilesystemRoot'])) {
-				$this->reports[] = GeneralUtility::makeInstance('TYPO3\CMS\Reports\Status',
+				$this->reports[] = GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status',
 					'Filesystem',
 					'Hash filesystem root directory exists',
 					'Directory for hash filesystem root (' . $extConfSettings['hashFilesystemRoot'] . ') exists.',
 					Status::OK
 				);
 				if (is_writable(PATH_site . '/' . $extConfSettings['hashFilesystemRoot'])) {
-					$this->reports[] = GeneralUtility::makeInstance('TYPO3\CMS\Reports\Status',
+					$this->reports[] = GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status',
 						'Filesystem',
 						'Hash filesystem root is writable',
 						'Directory for hash filesystem root (' . $extConfSettings['hashFilesystemRoot'] . ') is writable.',
 						Status::OK
 					);
 				} else {
-					$this->reports[] = GeneralUtility::makeInstance('TYPO3\CMS\Reports\Status',
+					$this->reports[] = GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status',
 						'Filesystem',
 						'Hash filesystem root is not writable',
 						'The directory for hash filesystem root (' . $extConfSettings['hashFilesystemRoot'] . ') is not writable!',
@@ -141,7 +141,7 @@ class Tx_Yag_Report_Filesystem implements \TYPO3\CMS\Reports\StatusProviderInter
 					);
 				}
 			} else {
-				$this->reports[] = GeneralUtility::makeInstance('TYPO3\CMS\Reports\Status',
+				$this->reports[] = GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status',
 					'Filesystem',
 					'Hash filesystem root does not exist',
 					'The directory for hash filesystem root (' . $extConfSettings['hashFilesystemRoot'] . ') does not exist!',
@@ -149,7 +149,7 @@ class Tx_Yag_Report_Filesystem implements \TYPO3\CMS\Reports\StatusProviderInter
 				);
 			}
 		} else {
-			$this->reports[] = GeneralUtility::makeInstance('TYPO3\CMS\Reports\Status',
+			$this->reports[] = GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status',
 				'Filesystem',
 				'Hash filesystem root is not set',
 				'The directory for hash filesystem root has not been set in Extension Manager!',
