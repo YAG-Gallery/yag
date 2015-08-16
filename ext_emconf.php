0695312e04f4762de0914309ba99adb419e48644
Daniel Lienert <daniel@lienert.cc>
2015-08-16 18:26:27 +0200

    [TASK] Set compatibility to 7.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array (
     array (
       'pt_extlist' => '2.0.4',
       'pt_extbase' => '2.0.0',
-      'typo3' => '6.2.0-7.1.99',
+      'typo3' => '6.2.0-7.4.99',
     ),
     'conflicts' => 
     array (
