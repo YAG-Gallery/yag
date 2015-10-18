446d9981a76240dd781f4fdbf25498e46fb30272
Daniel Lienert <daniel@lienert.cc>
2015-10-18 16:04:20 +0200

    [BUGFIX] Remove unused language / versioning fields from tx_yag_domain_model_resolutionfilecache

--- Configuration/TCA/tx_yag_domain_model_resolutionfilecache.php
+++ Configuration/TCA/tx_yag_domain_model_resolutionfilecache.php
@@ -25,52 +25,6 @@ return array(
         '1' => array('showitem' => ''),
     ),
     'columns' => array(
-        'sys_language_uid' => array(
-            'exclude' => 1,
-            'label' => 'LLL:EXT:lang/locallang_general.php:LGL.language',
-            'config' => array(
-                'type' => 'select',
-                'foreign_table' => 'sys_language',
-                'foreign_table_where' => 'ORDER BY sys_language.title',
-                'items' => array(
-                    array('LLL:EXT:lang/locallang_general.php:LGL.allLanguages', -1),
-                    array('LLL:EXT:lang/locallang_general.php:LGL.default_value', 0)
-                ),
-            )
-        ),
-        'l18n_parent' => array(
-            'displayCond' => 'FIELD:sys_language_uid:>:0',
-            'exclude' => 1,
-            'label' => 'LLL:EXT:lang/locallang_general.php:LGL.l18n_parent',
-            'config' => array(
-                'type' => 'select',
-                'items' => array(
-                    array('', 0),
-                ),
-                'foreign_table' => 'tx_yag_domain_model_resolutionfilecache',
-                'foreign_table_where' => 'AND tx_yag_domain_model_resolutionfilecache.uid=###REC_FIELD_l18n_parent### AND tx_yag_domain_model_resolutionfilecache.sys_language_uid IN (-1,0)',
-            )
-        ),
-        'l18n_diffsource' => array(
-            'config' => array(
-                'type' => 'passthrough',
-            )
-        ),
-        't3ver_label' => array(
-            'displayCond' => 'FIELD:t3ver_label:REQ:true',
-            'label' => 'LLL:EXT:lang/locallang_general.php:LGL.versionLabel',
-            'config' => array(
-                'type' => 'none',
-                'cols' => 27,
-            )
-        ),
-        'hidden' => array(
-            'exclude' => 1,
-            'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.hidden',
-            'config' => array(
-                'type' => 'check',
-            )
-        ),
         'paramhash' => array(
             'exclude' => 0,
             'label' => 'LLL:EXT:yag/Resources/Private/Language/locallang_db.xlf:tx_yag_domain_model_resolutionfilecache.paramhash',
