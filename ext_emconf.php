d05f657dcbac7821a2a45ffac84674b963ef244f
Daniel Lienert <daniel@lienert.cc>
2015-10-18 15:09:38 +0200

    [TASK] Set minimum TYPO3 requirement to 7.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
     array(
       'pt_extlist' => '2.0.4',
       'pt_extbase' => '2.0.0',
-      'typo3' => '6.2.0-7.5.99',
+      'typo3' => '7.0.0-7.6.99',
     ),
     'conflicts' =>
     array(
